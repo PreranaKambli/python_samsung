43361557f373b43a715efac2a7415244310a794c
PreranaKamblli <plkambli2005@gmail.com>
2024-12-10 16:17:51 +0530

    if else programs

--- python_building_blocks/leapyear.py
+++ python_building_blocks/leapyear.py
@@ -1,4 +1,9 @@
 #check if user given year is a leap year or no
 
-print("Enter the year to check if it is a leap year or not")
-input_year
+print("Enter a year to check if it is a leap year or not: ")
+input_year = int(input())
+
+if input_year % 4 == 0:
+    print(input_year,"It is a leap year")
+else:
+    print(input_year,"It is not a leap year")
