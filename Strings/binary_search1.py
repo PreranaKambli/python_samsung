44a9a0ebe2d82daf7d71d07fab29b022c2f015ba
PreranaKamblli <plkambli2005@gmail.com>
2024-12-15 11:01:16 +0530

    binary search pgms

--- Strings/binary_search1.py
+++ Strings/binary_search1.py
@@ -1,19 +1,31 @@
-
 import sys
-def binary_searh(numbers,search_key):
-     
-size=int(sys.argv[1])
-number = []
-search_key = 0
-print(f'enter {size} number of the array')
-for i in range (size):
-     number.append(int(input()))
-search_key=int(input("enter the element to be searched"))     
-index = binary_search(number,search_key)
+
+def binary_search(numbers, search_key):
+    low = 0
+    high = len(numbers) - 1
+    while(low <= high):
+        mid = (low + high) // 2
+        if numbers[mid] == search_key:
+            return mid
+        elif search_key < numbers[mid]:
+            high = mid - 1
+        else:
+            low = mid + 1
+    return -1
+
+size = int(sys.argv[1])  #program starts here
+numbers = []
+
+print(f'Enter {size} numbers of the array')
+for i in range(size):
+    numbers.append(int(input()))
+
+numbers.sort()
+search_key = int(input('Enter the element to be searched: '))
+
+index = binary_search(numbers, search_key)
+print('The inpur array is: \n', numbers)
 if index == -1:
-     print(f"the number {search_key} was not found")
+    print(f'The number {search_key} was not found')
 else:
-     print(f"the number{search_key}was found at the index{index}")
-
-
-
+    print(f'The number {search_key} was found at index {index}')
